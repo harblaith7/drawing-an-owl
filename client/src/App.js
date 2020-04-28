eada77c5a2731e0f6fb1df6c52283344cedb3c04
Laith Harb <harblaith77@gmail.com>
2020-04-27 21:40:20 -0400

    Completed header

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import WebCam from "./component/WebCam/WebCam"
 import './App.scss';
+import LandingPage from "./pages/LandingPage/LandingPage"
 
 function App() {
   return (
     <div className="App">
-        <WebCam/>
+        <LandingPage/>
     </div>
   );
 }
---
+++ client/src/component/Header/Header.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import "./Header.scss"
+import laithSide from "../../assets/laith-side.png"
+import andrewSide from "../../assets/andrew-side.png"
+import shopify from "../../assets/shopify_logo.png"
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="Header">
+                <div className="Header__container">
+                    <div className="Header__text-container">
+                        <div className="Header__text-content-container">
+                            <h1 className="Header__heading">Hey <img src={shopify} alt="" className="Header__logo"/></h1>
+                            <h3 className="Header__sub-heading">
+                                We're <span>Laith</span> & <span>Andrew</span> and we drew
+                                that <span className="yellow">f**king</span> <span>owl</span>!
+                            </h3>
+                            <button className="Header__btn">
+                                Check it out
+                            </button>
+                        </div>
+                    </div>
+                    <div className="Header__img-container">
+                        <img src={laithSide} alt="" className="Header__image"/>
+                        <img src={andrewSide} alt="" className="Header__image Header__image--andrew-side"/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
